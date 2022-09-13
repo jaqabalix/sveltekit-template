3bc6d849d666820dc3430342c381f287283e8e50
jaqabalix@gmail.com <jaqabalix@gmail.com>
2022-09-12 18:37:11 -0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    let name = 'Juan'
+</script>
+
+<div class="flex justify-center items-center p-10 w-screen h-screen prose max-w-none">
+    <h1>
+        Hello {name}!
+    </h1>
+</div>
